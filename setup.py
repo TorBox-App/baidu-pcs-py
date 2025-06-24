cf892446630127479a2f8f0558033280a3d57b47
Wamy <25761442+Wamy-Dev@users.noreply.github.com>
2025-06-24 02:49:58 -0500

    fix: includes pyx

--- setup.py
+++ setup.py
@@ -5,4 +5,15 @@
 from setuptools import setup, find_packages
 
 if __name__ == "__main__":
-    setup(name="baidupcs-py", version="0.7.6", packages=find_packages(exclude=["imgs"]))
+    setup(
+        name="baidupcs-py", 
+        version="0.7.6", 
+        packages=find_packages(exclude=["imgs"]), 
+        package_data={
+            "": ["*.pyx"],  # Include all .pyx files
+            "baidupcs_py": ["**/*.pyx"],  # Include .pyx files in subdirectories
+            "common": ["*.pyx", "**/*.pyx"],  # Include .pyx in common folder
+            "baidupcs_py.common": ["*.pyx", "**/*.pyx"],  # Include .pyx in common folder
+        },
+        include_package_data=True,
+    )
