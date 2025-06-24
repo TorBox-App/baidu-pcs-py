3f501cd67af4e2a0674ea136bf32270026f494f2
Wamy <25761442+Wamy-Dev@users.noreply.github.com>
2025-06-24 02:43:34 -0500

    fix: proper setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 # This is a shim to hopefully allow Github to detect the package, build is done with poetry
 
-import setuptools
+from setuptools import setup, find_packages
 
 if __name__ == "__main__":
-    setuptools.setup(name="baidupcs-py")
+    setup(name="baidupcs-py", version="0.7.6", packages=find_packages(exclude=["imgs"]))
